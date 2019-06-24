031c6fbd628c418c44f2df3fe963ebd9fdafa7ca
wxj <1123854718@qq.com>
2019-06-24 17:56:07 +0800

    1

---
+++ 王行建/src/views/addressEdit.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <Head title="地址编辑"></Head>
+    <div class="address">
+      <van-address-edit
+        :area-list="areaList"
+        show-postal
+        show-set-default
+        show-search-result
+        :search-result="searchResult"
+        @save="onSave"
+        @change-detail="onChangeDetail"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import areaList from "../utils/data.js";
+import { Toast } from "vant";
+export default {
+  data() {
+    return {
+      areaList,
+      searchResult: []
+    };
+  },
+  methods: {
+    onSave(content) {
+      var myMsg = content;
+      this.$axios.post("/vue/address", myMsg).then(res => {
+        Toast(res.data.msg);
+        if (!!res.data.type) {
+          this.$router.push({ path: "/address" });
+        }
+      });
+    },
+    onChangeDetail(val) {
+      if (val) {
+        this.searchResult = [
+          {
+            name: "黄龙万科中心",
+            address: "杭州市西湖区"
+          }
+        ];
+      } else {
+        this.searchResult = [];
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.address {
+  margin-top: 0.8rem;
+}
+</style>
---
+++ 王行建/src/views/guide.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <van-swipe :autoplay="0" indicator-color="white">
+      <van-swipe-item v-for="(img,i) in imgs" :key="i">
+        <img :src="img" alt class="swiper" @click="gotoHome(i)">
+      </van-swipe-item>
+    </van-swipe>
+  </div>
+</template>
+<script>
+export default {
+  name: "guide",
+  data() {
+    return {
+      imgs: [
+        require("@/assets/images/slide1.png"),
+        require("@/assets/images/slide2.png"),
+        require("@/assets/images/slide3.png"),
+        require("@/assets/images/slide4.png")
+      ]
+    };
+  },
+  methods: {
+    gotoHome(i) {
+      if (i == this.imgs.length - 1) {
+        this.$router.push({ name: "home" });
+      }
+    }
+  },
+  mounted() {
+    if (localStorage.count) {
+      localStorage.count++;
+      if (localStorage.count > 2) {
+        this.$router.push({ name: "home" });
+      }
+    } else {
+      localStorage.count = 1;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.guide .swiper {
+  width: 100%;
+  height: 100%;
+}
+</style>
+
+
+
